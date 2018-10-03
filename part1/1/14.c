e7fa9f59b403703014457f9fd759b96cfdc07d5c
ss0x00 <37372738+ss0x00@users.noreply.github.com>
2018-10-03 21:45:31 +0900

    Update 14.c

--- part1/1/14.c
+++ part1/1/14.c
@@ -4,8 +4,8 @@ int main(void)
 {
 	int i, j;
 
-	for(i=1; i<=9; i++) {
-		for(j = 2; j <= 5; j++) {
+	for(i=2; i<=5; i++) {
+		for(j = 1; j <= 9; j++) {
 			printf("%d * %d = %d\t", j, i, i*j);		
 		}
 		printf("\n");
